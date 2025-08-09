80b57d7e8c28abebff9ee1dbefb9a1850df52241
Min Namgung <nmin1124@gmail.com>
2025-08-09 12:34:03 +0900

    Time: 11 ms (88.66%), Space: 42.9 MB (83.85%) - LeetHub

--- 0837-most-common-word/0837-most-common-word.java
+++ 0837-most-common-word/0837-most-common-word.java
@@ -1,13 +1,14 @@
+import java.util.*;
+
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
-        Set<String> banSet = new HashSet<>(Arrays.asList(banned));
+        Set<String> ban = new HashSet<>(Arrays.asList(banned));
         Map<String, Integer> counts = new HashMap<>();
-
         String[] words = paragraph.replaceAll("\\W+", " ").toLowerCase().split(" ");
 
-        for (String w : words) {
-            if (!banSet.contains(w)) {
-                counts.put(w, counts.getOrDefault(w, 0) + 1);
+        for (String word : words) {
+            if (!ban.contains(word)) {
+                counts.put(word, counts.getOrDefault(word, 0) + 1);
             }
         }
 
