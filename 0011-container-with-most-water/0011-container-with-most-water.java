a1835f56ae16dcd99d1de49cf3d1dca6c010af2f
Min Namgung <nmin1124@gmail.com>
2025-09-05 15:55:02 +0900

    Time: 5 ms (76.41%), Space: 58.1 MB (43.91%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -2,10 +2,11 @@ class Solution {
     public int maxArea(int[] height) {
         int left = 0;
         int right = height.length - 1;
+        int curWater = 0;
         int mostWater = 0;
 
         while (left < right) {
-            int curWater = Math.min(height[left], height[right]) * (right - left);
+            curWater = Math.min(height[left], height[right]) * (right - left);
             mostWater = Math.max(mostWater, curWater);
 
             if (height[left] < height[right]) left++;
