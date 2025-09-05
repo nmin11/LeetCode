6bb7bc070ded769a93a5bdb574d0501ed21e7bfe
Min Namgung <nmin1124@gmail.com>
2025-09-05 15:54:28 +0900

    Time: 6 ms (80.13%), Space: 61.1 MB (13.33%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.kt
+++ 0011-container-with-most-water/0011-container-with-most-water.kt
@@ -2,10 +2,11 @@ class Solution {
     fun maxArea(height: IntArray): Int {
         var left = 0
         var right = height.size - 1
+        var curWater = 0
         var mostWater = 0
 
         while (left < right) {
-            val curWater = minOf(height[left], height[right]) * (right - left)
+            curWater = minOf(height[left], height[right]) * (right - left)
             mostWater = maxOf(mostWater, curWater)
 
             if (height[left] < height[right]) left++
