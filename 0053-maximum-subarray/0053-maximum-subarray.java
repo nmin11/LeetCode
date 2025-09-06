84bece6a15b1c464b5f1efa152d03638cf948654
Min Namgung <nmin1124@gmail.com>
2025-09-06 15:19:46 +0900

    Time: 1 ms (99.5%), Space: 57.3 MB (31.55%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            maxSum = Math.max(maxSum + nums[i], nums[i]);
+            sum = Math.max(sum, maxSum);
+        }
+
+        return sum;
+    }
+}
