57cba262d7cd1ef35a3ffab5d96f9454812b8712
Min Namgung <nmin1124@gmail.com>
2025-10-03 14:57:28 +0900

    Time: 20 ms (20.28%), Space: 44.7 MB (44.38%) - LeetHub

---
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = head;
+
+        while (cur != null) {
+            ListNode next = cur.next;
+            ListNode prev = dummy;
+
+            while (prev.next != null && prev.next.val < cur.val) {
+                prev = prev.next;
+            }
+
+            cur.next = prev.next;
+            prev.next = cur;
+
+            cur = next;
+        }
+
+        return dummy.next;
+    }
+}
