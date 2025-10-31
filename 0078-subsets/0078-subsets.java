7716e7381b96f861b97f9fa1ebf0b0ae41185167
Min Namgung <nmin1124@gmail.com>
2025-10-31 14:32:51 +0900

    Time: 1 ms (48.35%), Space: 43.3 MB (5.12%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,18 @@
+class Solution {
+    private void dfs(List<List<Integer>> results, int[] nums, int index, Deque<Integer> path) {
+        results.add(new ArrayList<>(path));
+        
+        for (int i = index; i < nums.length; i++) {
+            path.add(nums[i]);
+            dfs(results, nums, i + 1, path);
+            path.removeLast();
+        }
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        dfs(results, nums, 0, new ArrayDeque<>());
+
+        return results;
+    }
+}
