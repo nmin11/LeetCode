1e32a20c58d77321f8a6dced46d642e799dedfbd
Min Namgung <nmin1124@gmail.com>
2025-12-10 13:32:10 +0900

    Time: 16 ms (94.73%), Space: 220.4 MB (68.82%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -0,0 +1,30 @@
+class Solution {
+    private static int[] parent;
+
+    private static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    private static void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) {
+            parent[rootY] = rootX;
+        }
+    }
+
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        for (int[] edge : edges) {
+            union(edge[0], edge[1]);
+        }
+
+        return find(source) == find(destination);
+    }
+}
