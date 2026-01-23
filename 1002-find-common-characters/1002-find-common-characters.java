34b793453162969f7d533696d0c8613dd07d7dea
Min Namgung <nmin1124@gmail.com>
2026-01-24 01:41:09 +0900

    Time: 3 ms (91.36%), Space: 44.7 MB (56.87%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<String> commonChars(String[] words) {
+        int[] minFreq = new int[26];
+        for (char c : words[0].toCharArray()) {
+            minFreq[c - 'a']++;
+        }
+
+        for (int i = 1; i < words.length; i++) {
+            int[] freq = new int[26];
+            for (char c : words[i].toCharArray()) {
+                freq[c - 'a']++;
+            }
+
+            for (int j = 0; j < 26; j++) {
+                minFreq[j] = Math.min(minFreq[j], freq[j]);
+            }
+        }
+
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            for (int count = 0; count < minFreq[i]; count++) {
+                result.add(String.valueOf((char) (i + 'a')));
+            }
+        }
+
+        return result;
+    }
+}
