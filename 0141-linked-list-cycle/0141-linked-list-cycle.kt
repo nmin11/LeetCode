1764d624e3ceada432855a0100ace3b153e858ca
Min Namgung <nmin1124@gmail.com>
2026-02-02 01:46:15 +0900

    Time: 125 ms (96.98%), Space: 45.8 MB (61.45%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.kt
@@ -0,0 +1,35 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun hasCycle(head: ListNode?): Boolean {
+        var walker = head
+        var runner = head
+
+        while (runner != null) {
+            runner = runner.next
+
+            if (runner != null) {
+                runner = runner.next
+                walker = walker?.next
+
+                if (runner == walker) break
+            } else {
+                break
+            }
+        }
+
+        if (runner == null) {
+            return false
+        } else {
+            return true
+        }
+    }
+}
