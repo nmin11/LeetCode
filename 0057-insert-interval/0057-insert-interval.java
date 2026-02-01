fb97e9455a7eda5d5fada099aedff38b66500a02
Min Namgung <nmin1124@gmail.com>
2026-02-01 14:43:00 +0900

    Time: 1 ms (97.66%), Space: 47.2 MB (29%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+
+        int start = newInterval[0];
+        int end = newInterval[1];
+        int i = 0;
+        int n = intervals.length;
+
+        while (i < n && intervals[i][1] < start) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        while (i < n && intervals[i][0] <= end) {
+            start = Math.min(start, intervals[i][0]);
+            end = Math.max(end, intervals[i][1]);
+            i++;
+        }
+        result.add(new int[]{start, end});
+
+        while (i < n) {
+            result.add(intervals[i]);
+            i++;
+        }
+        
+        return result.toArray(new int[result.size()][]);
+    }
+}
