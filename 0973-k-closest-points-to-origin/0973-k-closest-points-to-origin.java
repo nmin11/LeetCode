33bd70c3e1fd64bef32deceb48e977c15ff99ce1
Min Namgung <nmin1124@gmail.com>
2026-02-04 01:00:33 +0900

    Time: 20 ms (94.11%), Space: 59.5 MB (12.27%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            int distA = a[0] * a[0] + a[1] * a[1];
+            int distB = b[0] * b[0] + b[1] * b[1];
+            return distB - distA;
+        });
+
+        for (int[] point : points) {
+            pq.offer(point);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        return pq.toArray(new int[k][]);
+    }
+}
