1e55b2630ddda9d468255716f5e081e0508a1c58
Min Namgung <nmin1124@gmail.com>
2026-02-07 00:38:33 +0900

    Time: 33 ms (37.34%), Space: 49.7 MB (69.71%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.kt
@@ -0,0 +1,37 @@
+class MinStack() {
+    private val stack = ArrayDeque<Int>()
+    private val minStack = ArrayDeque<Int>()
+
+    fun push(`val`: Int) {
+        stack.addLast(`val`)
+
+        if (minStack.isEmpty()) {
+            minStack.addLast(`val`)
+        } else {
+            minStack.addLast(minOf(`val`, minStack.last()))
+        }
+    }
+
+    fun pop() {
+        stack.removeLast()
+        minStack.removeLast()
+    }
+
+    fun top(): Int {
+        return stack.last()
+    }
+
+    fun getMin(): Int {
+        return minStack.last()
+    }
+
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = MinStack()
+ * obj.push(`val`)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
