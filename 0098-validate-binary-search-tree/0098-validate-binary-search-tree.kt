d10470937148dd87b2ff8d9fa98da24911cd1d6a
Min Namgung <nmin1124@gmail.com>
2026-02-08 00:35:33 +0900

    Time: 0 ms (100%), Space: 45 MB (65.25%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.kt
@@ -0,0 +1,23 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun isValidBST(root: TreeNode?): Boolean {
+        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE)
+    }
+
+    private fun isValid(node: TreeNode?, min: Long, max: Long): Boolean {
+        if (node == null) return true
+
+        if (node.`val` <= min || node.`val` >= max) return false
+
+        return isValid(node.left, min, node.`val`.toLong()) && isValid(node.right, node.`val`.toLong(), max)
+    }
+}
