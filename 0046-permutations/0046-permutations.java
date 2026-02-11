a7db2705adea3b1c8b6456771369b2e26fd9a5c7
Min Namgung <nmin1124@gmail.com>
2026-02-12 00:08:48 +0900

    Time: 1 ms (86.68%), Space: 45.5 MB (62.63%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,25 +1,26 @@
 class Solution {
-    private void dfs(List<List<Integer>> results, List<Integer> prevElements, List<Integer> elements) {
-        if (elements.isEmpty()) {
-            results.add(prevElements.stream().collect(Collectors.toList()));
-        }
-
-        for (Integer e : elements) {
-            List<Integer> nextElements = new ArrayList<>(elements);
-            nextElements.remove(e);
-            prevElements.add(e);
-
-            dfs(results, prevElements, nextElements);
-            prevElements.remove(e);
-        }
-    }
-
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> results = new ArrayList<>();
-        List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
-
-        dfs(results, new ArrayList<>(), list);
-
+        backtrack(results, new ArrayList<>(), nums, new boolean[nums.length]);
         return results;
+    }
+
+    private void backtrack(List<List<Integer>> results, List<Integer> cur, int[] nums, boolean[] used) {
+        if (cur.size() == nums.length) {
+            results.add(new ArrayList<>(cur));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+
+            cur.add(nums[i]);
+            used[i] = true;
+
+            backtrack(results, cur, nums, used);
+
+            cur.remove(cur.size() - 1);
+            used[i] = false;
+        }
     }
 }
