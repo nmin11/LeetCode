79a8bcad052f2414a70d9ef2a90c294f48754d26
Min Namgung <nmin1124@gmail.com>
2026-02-13 00:28:36 +0900

    Time: 167 ms (35.31%), Space: 48.7 MB (12.13%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.kt
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int = 0) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+        if (root == null) return null
+        if (root == p) return p
+        if (root == q) return q
+
+        val left = lowestCommonAncestor(root.left, p, q)
+        val right = lowestCommonAncestor(root.right, p, q)
+
+        if (left != null && right != null) return root
+
+        return left ?: right
+    }
+}
