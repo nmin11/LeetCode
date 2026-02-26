68b25a02dd580b2b84dcf3ef5c23a3994c8c031e
Min Namgung <nmin1124@gmail.com>
2026-02-27 01:08:36 +0900

    Time: 0 ms (100%), Space: 42.4 MB (78.64%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder sb = new StringBuilder();
+
+        while (columnNumber > 0) {
+            columnNumber--;
+            char c = (char)(columnNumber % 26 + 'A');
+            sb.append(c);
+            columnNumber /= 26;
+        }
+
+        return sb.reverse().toString();
+    }
+}
