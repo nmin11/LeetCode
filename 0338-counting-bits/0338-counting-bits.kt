1d88145f7a8648af89ab2ff06d1682cbd5bdd88e
Min Namgung <nmin1124@gmail.com>
2026-04-05 00:02:12 +0900

    Time: 1 ms (100%), Space: 51.6 MB (39.88%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun countBits(n: Int): IntArray {
+        val result = IntArray(n + 1)
+
+        for (i in 1..n) {
+            result[i] = result[i shr 1] + (i and 1)
+        }
+
+        return result
+    }
+}
