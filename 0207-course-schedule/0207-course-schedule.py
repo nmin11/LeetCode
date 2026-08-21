b4ad3156a2b6d1ef8918e4ada83ea063f2aa5897
Min Namgung <nmin1124@gmail.com>
2026-08-22 02:16:49 +0900

    Time: 4 ms (64.4%), Space: 20.3 MB (78.34%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+
+        for [a, b] in prerequisites:
+            graph[b].append(a)
+            indegree[a] += 1
+        
+        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+        completed = 0
+
+        while queue:
+            cur = queue.popleft()
+            completed += 1
+
+            for nxt in graph[cur]:
+                indegree[nxt] -= 1
+
+                if indegree[nxt] == 0:
+                    queue.append(nxt)
+        
+        return completed == numCourses
+        
